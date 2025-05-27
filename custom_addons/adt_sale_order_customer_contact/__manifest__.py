e2b0edfed87cf2c00e43714e0a6c3d6d10cb4622
Phuc Nguyen <phuclx@gmail.com>
2025-05-27 16:59:53 +0700

    Phuc/custom UI (#8)
    * Inventory - Remove Additional Info header
    
    * stock_picking_extension Show full address
    
    * Add sale_order_customer_contact
    
    * stock_picking_extension Refactor name
    
    * Refacter name
    
    * Show mobile in shipping address in PDF

--- custom_addons/adt_sale_order_customer_contact/__manifest__.py
+++ custom_addons/adt_sale_order_customer_contact/__manifest__.py
@@ -1,12 +1,13 @@
 {
     'name': 'ADT Sale Order Customer Contact',
-    'version': '1.2',
+    'version': '1.3',
     'summary': 'Show customer phone and mobile in sales order for ADT',
     'author': 'ADT',
     'category': 'Sales',
     'depends': ['sale', 'contacts'],
     'data': [
         'views/sale_order_views.xml',
+        'views/sale_order_report_templates.xml',
     ],
     'installable': True,
     'application': False,
