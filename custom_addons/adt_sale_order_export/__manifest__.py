9425a2a743cbcbc41aa7949b7b9454a557278337
Phuc Nguyen <phuclx@gmail.com>
2025-06-02 08:35:40 +0700

    Misa Export (#12)

---
+++ custom_addons/adt_sale_order_export/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'ADT Sale Order Export for Misa',
+    'version': '1.2',
+    'summary': 'Custom Sales Orders export for Misa',
+    'category': 'Sales',
+    'author': 'ADT',
+    'depends': ['sale'],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
