249afc09e73c415c7c8fa095949a4a31eca13d65
Phuc Nguyen <phuclx@gmail.com>
2025-06-04 21:17:46 +0700

    Update partner.ref.counter

--- custom_addons/adt_partner_reference/models/res_partner.py
+++ custom_addons/adt_partner_reference/models/res_partner.py
@@ -1,6 +1,4 @@
 from odoo import api, fields, models
-from datetime import datetime
-
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
@@ -8,12 +6,7 @@ class ResPartner(models.Model):
     ref = fields.Char(
         string="Reference",
         copy=False,
-        default=lambda self: (
-            "OHC{}{}".format(
-                datetime.now().strftime("%Y%m%d%H%M"),
-                self.env["ir.sequence"].next_by_code("partner.ref.counter"),
-            )
-        ),
+        default=lambda self: self.env["ir.sequence"].next_by_code("partner.ref.counter"),
         help="Unique MISA partner reference generated automatically.",
     )
 
@@ -21,12 +14,10 @@ class ResPartner(models.Model):
     def create(self, vals_list):
         """
         When new partners are created (even in batch/imports), if 'ref' was not explicitly given
-        in vals, assign the next value from ir.sequence('partner.ref').
+        in vals, assign the next value from ir.sequence('partner.ref.counter').
         """
         for vals in vals_list:
             # If the user did not supply a 'ref' manually, fetch from the sequence:
             if not vals.get("ref"):
-                ts = datetime.now().strftime("%Y%m%d%H%M")
-                tail = self.env["ir.sequence"].next_by_code("partner.ref.counter")
-                vals["ref"] = f"OHC{ts}{tail}"
+                vals["ref"] = self.env["ir.sequence"].next_by_code("partner.ref.counter")
         return super(ResPartner, self).create(vals_list)
