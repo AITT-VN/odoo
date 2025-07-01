0dd975c7d2a786ad098277191b018f62b03d82de
BuiTranTrucHa <habui0333@gmail.com>
2025-07-01 09:59:08 +0000

    Rename custom field expense_ref to reference

--- custom_addons/adt_expenses_reference/models/hr_expenses_reference.py
+++ custom_addons/adt_expenses_reference/models/hr_expenses_reference.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 class HrExpense(models.Model):
     _inherit = "hr.expense"
 
-    expense_ref = fields.Char(
+    reference = fields.Char(
         string="Reference", required=True, readonly=True, copy=False, default="New"
     )
 
@@ -12,8 +12,8 @@ class HrExpense(models.Model):
     def create(self, vals_list):
         for vals in vals_list:
             # Không tạo lại mã nếu đã có
-            if not vals.get("expense_ref") or vals["expense_ref"] == "New":
-                vals["expense_ref"] = self.env["ir.sequence"].next_by_code(
+            if not vals.get("reference") or vals["reference"] == "New":
+                vals["reference"] = self.env["ir.sequence"].next_by_code(
                     "hr.expense.reference"
                 )
         return super().create(vals_list)
