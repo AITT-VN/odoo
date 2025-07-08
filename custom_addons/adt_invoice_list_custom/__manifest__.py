8a5f5f89057b063196a7986161870aa35336674a
BuiTranTrucHa <habui0333@gmail.com>
2025-07-08 15:08:58 +0000

    Add fields to invoice list view (Salesperson, Recipient Bank, Invoice Date, Amount Paid, Amount Due)

---
+++ custom_addons/adt_invoice_list_custom/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "ADT Invoice List Custom",
+    "version": "1.0",
+    "category": "Accounting",
+    "summary": "Show extra columns in invoice list view",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_inherit.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
