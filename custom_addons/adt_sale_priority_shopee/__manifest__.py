fd1d458efb1b1093510b517df0fe0b7659af0b2a
BuiTranTrucHa <habui0333@gmail.com>
2025-07-16 03:33:10 +0000

    Add priority shopee oders

---
+++ custom_addons/adt_sale_priority_shopee/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "ADT Shopee Priority Pickings",
+    "version": "18.0.1.0.0",
+    "summary": "Tự động đánh dấu sao vàng cho các phiếu xuất kho từ đơn hàng Shopee",
+    "author": "Bùi Trần Trúc Hà",
+    "category": "Warehouse",
+    "depends": ["sale", "stock"],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
